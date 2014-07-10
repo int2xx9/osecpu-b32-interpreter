7172d863e0fdba5ab739fc4841a21d70b4987a93
int512 <github@int512.net>
2014-07-10 23:44:02 +0900

    First commit

---
+++ Makefile
@@ -0,0 +1,20 @@
+CC=gcc
+CFLAGS=
+MAKE=make --no-print-directory
+TARGET=osecpu
+SOURCE=main.c
+RM=rm -f
+
+.PHONY: default all clean
+default:
+	@$(MAKE) all
+
+all:
+	@$(MAKE) osecpu
+
+clean:
+	$(RM) $(TARGET)
+
+$(TARGET): $(SOURCE)
+	$(CC) -o $(TARGET) $(CFLAGS) main.c
+
---
+++ main.c
@@ -0,0 +1,215 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <inttypes.h>
+
+#define INSTRUCTION_LIMM 0x76000002
+#define INSTRUCTION_LIDR 0x760000fd
+
+struct Osecpu
+{
+	int registers[0x40];
+	int pregisters[0x40];
+	int dregisters[4];
+	uint8_t* code;
+	long codelen;
+
+	int invalid_instruction_error;
+};
+
+int load_b32(struct Osecpu* osecpu, const char* filename)
+{
+	FILE* fp;
+	long len;
+	uint8_t* code;
+
+	if (osecpu->code) return -1;
+
+	fp = fopen(filename, "rb");
+	if (!fp) return -1;
+
+	fseek(fp, 0, SEEK_END);
+	len = ftell(fp);
+	fseek(fp, 0, SEEK_SET);
+
+	// ファイルの長さが8バイト以下(シグネチャ長以下)はエラー
+	if (len <= 8) {
+		fclose(fp);
+		return -1;
+	}
+
+	code = (uint8_t*)malloc(len);
+	if (!code) {
+		fclose(fp);
+		return -1;
+	}
+
+	if (fread(code, len, 1, fp) < 1) {
+		free(code);
+		fclose(fp);
+		return -1;
+	}
+
+	fclose(fp);
+
+	// シグネチャチェック
+	if (strcmp(code, "\x05\xe2\x00\xcf\xee\x7f\xf1\x88") != 0) {
+		free(code);
+		return -1;
+	}
+
+	osecpu->code = (uint8_t*)malloc(len);
+	if (!osecpu->code) {
+		free(code);
+		return -1;
+	}
+
+	osecpu->codelen = len-8;
+	memcpy(osecpu->code, code+8, osecpu->codelen);
+
+	free(code);
+	return 0;
+}
+
+void coredump(struct Osecpu* osecpu)
+{
+	int i, j;
+
+	printf("Registers:\n");
+	i = 0;
+	while (i < 0x40/4) {
+		printf("R%02X: %08X\tR%02X: %08X\tR%02X: %08X\tR%02X: %08X\n"
+				, i+16*0, osecpu->registers[i+16*0]
+				, i+16*1, osecpu->registers[i+16*1]
+				, i+16*2, osecpu->registers[i+16*2]
+				, i+16*3, osecpu->registers[i+16*3]
+			  );
+		i++;
+	}
+	printf("\n");
+
+	printf("Registers(pointer):\n");
+	i = 0;
+	while (i < 0x40/4) {
+		printf("P%02X: %08X\tP%02X: %08X\tP%02X: %08X\tP%02X: %08X\n"
+				, i+16*0, osecpu->pregisters[i+16*0]
+				, i+16*1, osecpu->pregisters[i+16*1]
+				, i+16*2, osecpu->pregisters[i+16*2]
+				, i+16*3, osecpu->pregisters[i+16*3]
+			  );
+		i++;
+	}
+	printf("\n");
+
+	printf("Registers(debug):\n");
+	for (i = 0; i < 4; i++) {
+		printf("DR%X: %08X\t", i, osecpu->dregisters[i]);
+	}
+	printf("\n\n");
+
+	printf("osecpu->code:\n");
+	i = 0;
+	while (i < osecpu->codelen) {
+		printf("%08X: ", i);
+		for (j = 0; j < 16; j++) {
+			if (i+j > osecpu->codelen-1) break;
+			printf("%02X ", osecpu->code[i+j]);
+		}
+		printf("\n");
+		i+=j;
+	}
+}
+
+int fetch_code(struct Osecpu* osecpu)
+{
+	const int pc = osecpu->pregisters[0x3f];
+	if (pc+4 > osecpu->codelen) return 0;
+	osecpu->pregisters[0x3f] += 4;
+	return
+		(osecpu->code[pc+0] << 24) |
+		(osecpu->code[pc+1] << 16) |
+		(osecpu->code[pc+2] <<  8) |
+		(osecpu->code[pc+3] <<  0);
+}
+
+int do_instruction(struct Osecpu* osecpu, const int icode)
+{
+	switch (icode) {
+		case INSTRUCTION_LIMM:
+			{
+				const int resv = fetch_code(osecpu);
+				const int imm = fetch_code(osecpu);
+				const int r = fetch_code(osecpu)-0x76000000;
+				const int bit = fetch_code(osecpu)-0x76000000;
+				if (resv != 0xfffff788) return -1;
+				if (r < 0 || r > 0x3f) return -1;
+				if (bit != 0x20) return -1;
+				osecpu->registers[r] = imm;
+			}
+			break;
+		case INSTRUCTION_LIDR:
+			{
+				const int resv = fetch_code(osecpu);
+				const int imm = fetch_code(osecpu);
+				const int dr = fetch_code(osecpu)-0x76000000;
+				if (resv != 0xfffff788) return -1;
+				if (dr < 0 || dr > 4) return -1;
+				osecpu->dregisters[dr] = imm;
+			}
+			break;
+		default:
+			osecpu->invalid_instruction_error = icode;
+			break;
+	}
+	return 0;
+}
+
+int run_b32(struct Osecpu* osecpu)
+{
+	int icode;
+	while (1) {
+		icode = fetch_code(osecpu);
+		if (osecpu->codelen <= osecpu->pregisters[0x3f]) break;
+		do_instruction(osecpu, icode);
+		if(osecpu->invalid_instruction_error) return -1;
+	}
+	return 0;
+}
+
+
+int main(int argc, char** argv)
+{
+	struct Osecpu* osecpu;
+
+	if (argc < 2) {
+		printf("Usage: %s app.ose\n", argv[0]);
+		exit(EXIT_FAILURE);
+	}
+
+	osecpu = (struct Osecpu*)malloc(sizeof(struct Osecpu));
+	if (!osecpu) {
+		printf("malloc() failed\n");
+		exit(EXIT_FAILURE);
+	}
+	memset(osecpu, 0, sizeof(struct Osecpu));
+
+	if (load_b32(osecpu, argv[1]) == -1) {
+		printf("load_b32() error\n");
+		if (osecpu->code) free(osecpu->code);
+		free(osecpu);
+		exit(EXIT_FAILURE);
+	}
+
+	run_b32(osecpu);
+	if (osecpu->invalid_instruction_error) {
+		fprintf(stderr, "Error: invalid instruction error (%08X)\n", osecpu->invalid_instruction_error);
+	}
+
+	coredump(osecpu);
+
+	if (osecpu->code) free(osecpu->code);
+	free(osecpu);
+
+	return 0;
+}
+
